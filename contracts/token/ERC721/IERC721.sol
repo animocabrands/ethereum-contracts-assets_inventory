ba723fcf36eedbfec69169b87aa6080b3bc0b157
Jan-Paul Azucena <jp@fuelpowered.com>
2020-05-03 11:59:31 -0700

    [REFACTOR] Restoring inadvertent removal of indexing of the _tokenId from the Transfer and Approval events in the IERC721.sol interface file

--- contracts/token/ERC721/IERC721.sol
+++ contracts/token/ERC721/IERC721.sol
@@ -9,13 +9,13 @@ interface IERC721 {
     event Transfer(
         address indexed _from,
         address indexed _to,
-        uint256 _tokenId
+        uint256 indexed _tokenId
     );
 
     event Approval(
         address indexed _owner,
         address indexed _approved,
-        uint256 _tokenId
+        uint256 indexed _tokenId
     );
 
     event ApprovalForAll(
